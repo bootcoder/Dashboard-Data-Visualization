52fad73f8f78fead63c16869b494df3af1a0206b
Hunter Chapman <bootcoder@gmail.com>
2014-10-08 16:11:13 -0700

    add routes and db info

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  # root to: 'users#index' 
+  root to: 'charts#index' 
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
